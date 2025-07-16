d86dec4db753ee122717d0b2ed8cb2cfa1600242
KyleFontenot <kylewilliamfontenot@gmail.com>
2025-07-16 16:46:14 -0700

    Deployment adjustments

---
+++ README.md
@@ -0,0 +1,9 @@
+# To use:
+
+1. Make sure you have Node installed. ([fnm is great](https://github.com/Schniz/fnm) and easy. with fnm is installed, just run `fnm install 20` or 22, which Node version you want. )
+2. in the folder you want to clone this as a new folder: `git clone https://github.com/KyleFontenot/simplebuilder.git --depth=1`
+3. `cd simplebuilder` 
+4. `npm install` 
+5. `npm run build`
+
+Then your build application of static html files are in the `build` folder. You can simply upload that `build` folder to your hosting platform, or you can automate this process in platforms like Cloudflare by linking it to the git repositry so it rebuilds on every commit. 
--- build.ts
+++ build.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath } from 'node:url'
-import {readFileSync, readdirSync, lstatSync, copyFileSync, writeFileSync, mkdirSync} from "node:fs"
+import {readFileSync, readdirSync, lstatSync, copyFileSync, writeFileSync, mkdirSync, rmSync} from "node:fs"
 import {readFile } from "node:fs/promises"
 import {join, resolve, dirname} from "node:path"
 import { nanoid } from 'nanoid/non-secure'
@@ -31,12 +31,13 @@ function writeFileSyncRecursive(filePath: string , data : string, options = {}) 
   writeFileSync(filePath, data, options);
 }
 
-async function walkStructure(template: string){
+ function walkStructure(template: string){
+  rmSync(buildFolder, { recursive: true });
+
   const startpoint = "./src/routes/"
   
-  function beginWalk(start: string){
+  async function beginWalk(start: string){
     const absLocation = resolve(start);
-    console.log("aboslute llocation",absLocation)
     const treeLocation = start.replace(/^(?:\.\.?\/|\/)*(?:.*\.\.\/)*/, '');
 
     const walk = async (pathname: string) => {
@@ -68,6 +69,7 @@ async function walkStructure(template: string){
                   const injected = template.replace(/%body%/g, contentHtml)
                   try{
                     writeFileSyncRecursive(join(buildFolder, pathname, 'index.html'), injected)
+                    console.log(`Route made: ${pathname}`)
                   }
                   catch(e){
                     console.error(e)
@@ -82,7 +84,6 @@ async function walkStructure(template: string){
                 const contentMd = readFileSync(absoluteFile, "utf-8");
                 if(file.startsWith("+")){
                     const mdParsed = await marked.parse(contentMd);
-                    console.log("md parsed", mdParsed)
                     const injected = template.replace(/%body%/g, mdParsed)
                     writeFileSyncRecursive(join(buildFolder, pathname, 'index.html'), injected)
                   }
@@ -106,7 +107,9 @@ async function walkStructure(template: string){
       })
     );
     }
-    walk('');
+    walk('').then(d => {
+      console.log("Finished walking");
+    });
   }
 
   // Can keep track of all routes made as it walks
@@ -115,10 +118,11 @@ async function walkStructure(template: string){
   const filesInDir = beginWalk(startpoint);
 }
 
-function build(){
+async function build(){
  const template = constructBaseHTML();
  mkdirSync(buildFolder, { recursive: true });
- walkStructure(template)
+  walkStructure(template)
+  console.log("Done")
 }
 
-build()
+await build()
